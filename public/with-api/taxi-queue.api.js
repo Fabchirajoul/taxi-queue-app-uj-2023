09ae01beb7d67c987323430aef62eedcbe78cd2a
Fabchirajoul <fabchirajoul@gmail.com>
2023-08-21 14:44:51 +0200

    my taxi function with and without api

--- public/with-api/taxi-queue.api.js
+++ public/with-api/taxi-queue.api.js
@@ -4,40 +4,73 @@ document.addEventListener('alpine:init', () => {
 
 		return {
 			version: 'api-1.0',
-			queueLength: 0,
+			queueLen: 0,
+			taxiQueueLen: 0,
+
 			init() {
+				this.queueLength()
+				this.taxiQueueLength()
+			},
+
+			queueLength() {
 				axios
 					.get('/api/passenger/queue')
 					.then(result => {
 						// an example API call
 						this.queueLength = result.data.queueCount
+						console.log(result.data.queueCount)
 					});
 			},
+
 			joinQueue() {
-
+				axios
+					.post('/api/passenger/join')
+					.then((result) => {
+						console.log(result.data.message);
+					})
 			},
-			leaveQueue() {
 
+
+			leaveQueue() {
+				axios
+					.post('/api/passenger/leave')
+					.then((result) => {
+						console.log(result.data.message);
+					})
 			},
 
 			joinTaxiQueue() {
-
-			},
-
-			queueLength() {
-
+				axios
+					.post('/api/taxi/join')
+					.then((result) => {
+						console.log(result.data.message);
+					})
 			},
 
 			taxiQueueLength() {
+				axios
+					.get('/api/taxi/queue')
+					.then((result) => {
+						this.taxiQueueLen = result.data.queueCount
+					})
+			},
 
+			queueLength() {
+				axios
+					.get('/api/passenger/queue')
+					.then((result) => {
+						this.queueLen = result.data.queueCount
+					})
 			},
 
 			taxiDepart() {
-
+				axios
+					.post('/api/taxi/depart')
+					.then((result) => {
+						console.log(result.data.message);
+					})
 			}
 		}
 	});
 
 });
-
-
